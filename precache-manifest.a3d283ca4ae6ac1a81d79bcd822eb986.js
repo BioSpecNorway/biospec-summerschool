f9dd17bdb4ebaa796ec5ce1ad164dcfc53f603e9
Stanislau Trukhan <stas.truhan@gmail.com>
2019-02-09 22:33:52 +0100

    Updates

---
+++ precache-manifest.a3d283ca4ae6ac1a81d79bcd822eb986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eab66f2ea9dbcf949b1",
+    "url": "/biospec-summerschool/static/css/main.b779f88e.chunk.css"
+  },
+  {
+    "revision": "8eab66f2ea9dbcf949b1",
+    "url": "/biospec-summerschool/static/js/main.8eab66f2.chunk.js"
+  },
+  {
+    "revision": "8bae9b12353864342265",
+    "url": "/biospec-summerschool/static/css/1.71a31640.chunk.css"
+  },
+  {
+    "revision": "8bae9b12353864342265",
+    "url": "/biospec-summerschool/static/js/1.8bae9b12.chunk.js"
+  },
+  {
+    "revision": "6e5276e56e7bc0b8b342",
+    "url": "/biospec-summerschool/static/js/runtime~main.6e5276e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/biospec-summerschool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "992719ae87b2698a0391c52c936010c7",
+    "url": "/biospec-summerschool/index.html"
+  }
+];
