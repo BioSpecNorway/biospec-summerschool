17ff116ee75b50c3a16993f51aed1f23f2709b79
Stanislau Trukhan <stas.truhan@gmail.com>
2019-02-09 21:55:45 +0100

    Initial commit

---
+++ precache-manifest.5620ea58a2dc83188d6b489fe6460e18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "97f4cf2f68dc5144b96b",
+    "url": "/static/css/main.e982e452.chunk.css"
+  },
+  {
+    "revision": "97f4cf2f68dc5144b96b",
+    "url": "/static/js/main.97f4cf2f.chunk.js"
+  },
+  {
+    "revision": "8151f67e9bd4faf30b58",
+    "url": "/static/css/1.0fb5ca26.chunk.css"
+  },
+  {
+    "revision": "8151f67e9bd4faf30b58",
+    "url": "/static/js/1.8151f67e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ff2f6742dbc1088d67811978b51b52",
+    "url": "/index.html"
+  }
+];
