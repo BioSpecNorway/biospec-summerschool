e19e7dc6686c3aabb19c490e896f4014f64aec90
Stanislau Trukhan <stas.truhan@gmail.com>
2019-02-09 22:15:50 +0100

    Relative path for homepage

---
+++ precache-manifest.225233efce42fc2825819f251155c314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "01416b1478b4c45362e6",
+    "url": "./static/css/main.b779f88e.chunk.css"
+  },
+  {
+    "revision": "01416b1478b4c45362e6",
+    "url": "./static/js/main.01416b14.chunk.js"
+  },
+  {
+    "revision": "9b10eb7998b9de6554a3",
+    "url": "./static/css/1.71a31640.chunk.css"
+  },
+  {
+    "revision": "9b10eb7998b9de6554a3",
+    "url": "./static/js/1.9b10eb79.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "433097d2e1f681819c86711784ad9962",
+    "url": "./index.html"
+  }
+];
